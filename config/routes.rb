12ff220901b788903447f4ce293cdaaa6d1c4a80
nagichan0214 <ec2-user@ip-172-31-15-20.ap-northeast-1.compute.internal>
2024-03-08 07:25:04 +0000

    コメント機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -8,15 +8,18 @@ Rails.application.routes.draw do
   sessions: 'user/sessions'
 }
 
-# ゲストログイン
-devise_scope :user do
-  post "users/guest_sign_in", to: "user/sessions#guest_sign_in"
-end
+  # ゲストログイン
+  devise_scope :user do
+    post "users/guest_sign_in", to: "user/sessions#guest_sign_in"
+  end
 
-namespace :user, path: '' do
+  namespace :user, path: '' do
     root to: 'homes#top'
 
-    resources :posts,only: [:show, :edit, :index, :create, :new, :update, :destroy]
+    resources :posts,only: [:show, :edit, :index, :create, :new, :update, :destroy] do
+      resources :post_comments, only: [:create, :destroy]
+    end
+    
     resources :users, only: [:show, :edit, :index, :update]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
